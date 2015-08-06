c6a667a9e4cebb982cadedc3b89112b6aad7d509
DaiMysha <mlp.lemon.birdy@gmail.com>
2015-08-06 22:28:29 +0200

    Added a function to get the view AABB in the world's axes

--- include/DMUtils/sfml.hpp
+++ include/DMUtils/sfml.hpp
@@ -11,6 +11,7 @@ creation : 02/07/2015
 #ifndef HEADER_DMUTILS_SFML
 #define HEADER_DMUTILS_SFML
 
+#include <DMUtils/maths.hpp>
 #include <SFML/Graphics.hpp>
 
 namespace DMUtils {
@@ -31,8 +32,30 @@ namespace sfml {
     template <typename T>
     sf::Vector2<T> rotate(const sf::Vector2<T>& v, float alpha);
 
+    ///Rotates the point v around the origin o of alpha degrees
     template <typename T>
     sf::Vector2<T> rotate(const sf::Vector2<T>& v, float alpha, const sf::Vector2<T>& o);
+
+    ///Returns an AABB englobbing all the pixels visible in the selected view on the world coordinates
+	inline sf::IntRect getViewInWorldAABB(const sf::View& view) {
+        sf::Vector2f p[4];
+        sf::Vector2f c = view.getCenter();
+        sf::Vector2f s2 = view.getSize()/2.0f;
+        float alpha = view.getRotation();
+        p[0] = c - s2;
+        p[1] = c + sf::Vector2f(s2.x,-s2.y);
+        p[2] = c + s2;
+        p[3] = c + sf::Vector2f(-s2.x,s2.y);
+
+        for(int i=0;i<4;++i) p[i] = DMUtils::sfml::rotate(p[i],DMUtils::maths::degToRad(-alpha),c);
+
+        float xmin = DMUtils::maths::min(p[0].x,p[1].x,p[2].x,p[3].x);
+        float xmax = DMUtils::maths::max(p[0].x,p[1].x,p[2].x,p[3].x);
+        float ymin = DMUtils::maths::min(p[0].y,p[1].y,p[2].y,p[3].y);
+        float ymax = DMUtils::maths::max(p[0].y,p[1].y,p[2].y,p[3].y);
+
+        return sf::IntRect(sf::Vector2i(xmin,ymin),sf::Vector2i(xmax-xmin,ymax-ymin));
+    }
 
 }
 }
